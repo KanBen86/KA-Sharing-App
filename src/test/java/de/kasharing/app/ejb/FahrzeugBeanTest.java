b899d1cdfdacd86124931d8fd3dad2961b8c3cb2
KanBen86 <benjamin.kanzler@gmail.com>
2019-02-26 09:01:46 +0100

    Fehler auskommentiert

--- src/test/java/de/kasharing/app/ejb/FahrzeugBeanTest.java
+++ src/test/java/de/kasharing/app/ejb/FahrzeugBeanTest.java
@@ -5,7 +5,14 @@ package de.kasharing.app.ejb;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import de.kasharing.app.jpa.Fahrzeug;
+import java.util.Date;
+import javax.annotation.Resource;
+import javax.ejb.embeddable.EJBContainer;
+import javax.inject.Inject;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,24 +25,83 @@ import static org.junit.Assert.*;
  * @author Benjamin Kanzler
  */
 public class FahrzeugBeanTest {
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    private FahrzeugBean bean;
+
+    @Resource
+    private Fahrzeug f;
+
     public FahrzeugBeanTest() {
     }
-    
+
+    /*
     @BeforeClass
-    public static void setUpClass() {
+    public static void start() throws NamingException {
+        container = EJBContainer.createEJBContainer();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
+        if (container != null) {
+            container.close();
+        }
     }
-    
+    @Before
+    public void inject() throws NamingException {
+        container.getContext().bind("inject", this);
+    }
+
+    @After
+    public void reset() throws NamingException {
+        container.getContext().unbind("inject");
+    }
+     */
+
     @Before
     public void setUp() {
+
+        bean = new FahrzeugBean();
+
+        f = new Fahrzeug();
+
+        f.setAbs(true);
+        f.setAusfuehrung("Elegance");
+        f.setAnschaffungsPreis(11111.11f);
+        f.setAnschaffungsDatum(new Date());
+        f.setCd(true);
+        f.setElektrischeFensterheber(true);
+        f.setEsp(true);
+        f.setFahrassiSystem(true);
+        f.setHauptuntersuchungBis(new Date());
+        f.setHersteller("Nissan");
+        f.setKlimaanlage(true);
+        f.setLeihStatus("verliehen");
+        f.setModell("Tuscan");
+        f.setNavigation(true);
+        f.setPreisProTag(59.99f);
+        f.setRaeder(4);
+        f.setServolenkung(true);
+        f.setTyp("Kombi");
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
+    @Test
+    public void createFahrzeugTest() {
+
+        Fahrzeug updatedF = bean.updateFahrzeug(f);
+        assertTrue(updatedF.equals(f));
+    }
+    /*
+    @Test
+    public void findAllTest(){
+        System.out.println(bean.findAll());
+    }
+     */
 }
