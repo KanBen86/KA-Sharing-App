18517609ec2ec1dd43e4c3aa6ba9ffd0c9653595
KanBen86 <benjamin.kanzler@gmail.com>
2019-03-22 11:14:00 +0100

    Servlet auf die neue Nutzerstruktur anpassen

--- src/main/java/de/kasharing/app/web/RegistrierungServlet.java
+++ src/main/java/de/kasharing/app/web/RegistrierungServlet.java
@@ -6,13 +6,11 @@
 package de.kasharing.app.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import de.kasharing.app.jpa.Nutzer;
 import de.kasharing.app.jpa.Adresse;
 import de.kasharing.app.jpa.Bankverbindung;
 import javax.ejb.EJB;
@@ -20,9 +18,11 @@ import de.kasharing.app.ejb.MitarbeiterBean;
 import de.kasharing.app.ejb.KundeBean;
 import de.kasharing.app.ejb.BankverbindungBean;
 import de.kasharing.app.ejb.AdresseBean;
-import de.kasharing.app.enums.ResponseStatus;
 import de.kasharing.app.enums.NutzerRolle;
+import de.kasharing.app.enums.ResponseStatus;
 import de.kasharing.app.helper.Response;
+import de.kasharing.app.jpa.Kunde;
+import de.kasharing.app.jpa.Mitarbeiter;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -31,86 +31,90 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet(name = "RegistrierungServlet", urlPatterns = {"/register"})
 public class RegistrierungServlet extends HttpServlet {
-    
+
     private final static String URL = "/register/";
-    
-    @EJB
-    MitarbeiterBean mitarbeiterBean;
 
     @EJB
-    KundeBean kundeBean;
-    
+    protected MitarbeiterBean mitarbeiterBean;
+
     @EJB
-    BankverbindungBean bankverbindungBean;    
-    
+    protected KundeBean kundeBean;
+
     @EJB
-    AdresseBean adresseBean;
-    
+    protected BankverbindungBean bankverbindungBean;
+
+    @EJB
+    protected AdresseBean adresseBean;
+
+    private NutzerRolle nutzerRolle;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        NutzerRolle [] rollenList = nutzerRolle.values();
+
+        NutzerRolle[] rollenList = nutzerRolle.values();
         request.setAttribute("rollenList", rollenList);
-        
+
         request.getRequestDispatcher("/WEB-INF/detail.jsp").forward(request, response);
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         HttpSession s = request.getSession();
-        
+
         request.setCharacterEncoding("utf-8");
-        
+
         Adresse a = new Adresse();
         Bankverbindung bv = new Bankverbindung();
 
-   
         //Bankverbindungsdaten einlesen
-//        bv.setBic(request.getParameter("bic"));
-//        bv.setIban(request.getParameter("iban"));
-//        bv.setInsitut(request.getParameter("institut"));
-        
+        bv.setBic(request.getParameter("bic"));
+        bv.setIban(request.getParameter("iban"));
+        bv.setInsitut(request.getParameter("institut"));
+        // Persistieren der Bankverbindung
+        Response<Bankverbindung> responseBank = bankverbindungBean.findByBic(bv.getBic());
+        if (responseBank.getStatus()==ResponseStatus.NULL) {
+            responseBank = bankverbindungBean.createNewBankverbindung(bv);
+        }
         //Adressdaten einlesen
-//        a.setHausnummer(request.getParameter("hausnummer"));
-//        a.setName(request.getParameter("name"));
-//        a.setVorname(request.getParameter("vorname"));
-//        a.setOrt(request.getParameter("ort"));
-//        a.setPlz(request.getParameter("plz"));
-//        a.setStrasse(request.getParameter("strasse"));
+        a.setHausnummer(request.getParameter("hausnummer"));
+        a.setName(request.getParameter("name"));
+        a.setVorname(request.getParameter("vorname"));
+        a.setOrt(request.getParameter("ort"));
+        a.setPlz(request.getParameter("plz"));
+        a.setStrasse(request.getParameter("strasse"));
+        
+        Response<Adresse> responseAdresse = adresseBean.createNewAdresse(a);
         
         //Nutzerdaten einlesen und um Adresse und Bankverbindung ergänzen
 //        n.setAdresse(a);
 //        n.setBank(bv);
-
-        if((request.getParameter("nutzer")) == 1){
+        if (Integer.parseInt(request.getParameter("nutzer")) == 1) {
             Kunde k = new Kunde();
             k.setEmail(request.getParameter("email"));
             k.setNickName(request.getParameter("nickName"));
             k.setPasswort(request.getParameter("passwort"));
-            k = kundeBean.createNutzer(k);
-            
-        }
-        if else((request.getParameter("nutzer")) == 2){
+            Response<Kunde> responseK = new Response<>();
+            responseK = kundeBean.createNutzer(k);
+
+        } else if (Integer.parseInt(request.getParameter("nutzer")) == 2) {
             Mitarbeiter m = new Mitarbeiter();
             m.setEmail(request.getParameter("email"));
             m.setNickName(request.getParameter("nickName"));
             m.setPasswort(request.getParameter("passwort"));
-            m = mitarbeiterBean.createNutzer(m);
-        }
-        else{
+            m.setAdresse(responseAdresse.getResponse());
+            
+            Response<Mitarbeiter> responseM = new Response<>();
+            responseM = mitarbeiterBean.createNutzer(m);
+        } else {
             System.out.println("Nutzerrolle wurde nicht ausgefüllt!");
         }
-        
-        
+
 //        a = adresseBean.createNewAdresse(a).getResponse();
 //        bv = bankverbindungBean.createNewBankverbindung(bv).getResponse();
 //        n = nutzerBean.createNutzer(n);
-       
-        
         response.sendRedirect(request.getContextPath() + "/index.html");
     }
 
